94af72b283f2fda81805afaaee091dac938f37a9
Konsul84 <konsklein@web.>
2025-05-03 12:21:32 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,6 @@ import java.util.Iterator;
 public class Main {
     public static void main(String[] args) {
         CurrencyListFetcher fetcher = new CurrencyListFetcher();
-
         
-
     }
 }
