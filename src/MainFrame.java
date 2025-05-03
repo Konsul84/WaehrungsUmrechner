8781d5849eb93b762a200428d61c6e5146da0a1d
Konsul84 <konsklein@web.>
2025-05-03 21:12:07 +0200

    Jlabels für Währugscode hinzugefügt
    JLabels mit Währungscode erstellt
    initializeCurrencyLabels(); erstellt um die Labels zu aktualisieren
    Fensterüberschrift "Währumgsumrechner" hinzugefügt

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -9,7 +9,9 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 public class MainFrame extends JFrame {
     public JComboBox comboBoxFromCurrencies;
@@ -17,9 +19,13 @@ public class MainFrame extends JFrame {
     public JPanel mainPanel;
     private JTextField textFieldCurrValueFrom;
     private JTextField textFieldCurrValueTo;
+    private JLabel fromCurrencys;
+    private JLabel toCurrencys;
 
 
+    Map<String,String>map =new HashMap<>();
     public MainFrame() {
+        super("Währungsumrechner");
         setContentPane(mainPanel);
         setVisible(true);
         setSize(800, 600);
@@ -33,15 +39,28 @@ public class MainFrame extends JFrame {
             while (keys.hasNext()) {
                 String code = keys.next();
                 String name = currencies.getString(code);
-                comboBoxFromCurrencies.addItem(code);
+
                 // label setzen
-                comboBoxToCurrencies.addItem(code);
+                comboBoxFromCurrencies.addItem(name);
                 // label setzen
+                comboBoxToCurrencies.addItem(name);
+
+                // code name Paar in Map speichern
+                map.put(name,code);
             }
         }catch (Exception e) {
             System.out.println(e.getMessage());
+
         }
+        // Setzt die WährungsCOde Labels auf die Währung auch bevor eine eingabe gemacht wurde
+        initializeCodeCurrencyLabels();
         comboBoxFromCurrencies.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                ConvertCurrency();
+            }
+        });
+        comboBoxToCurrencies.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent e) {
                 ConvertCurrency();
@@ -63,10 +82,33 @@ public class MainFrame extends JFrame {
     }
 
 
+    // Setzt beim erstellen des Frames die COdeLabels auf die richtige Währung
+    private void initializeCodeCurrencyLabels() {
+
+        String nameFrom = comboBoxFromCurrencies.getSelectedItem().toString();
+        String nameTo =comboBoxToCurrencies.getSelectedItem().toString();
+
+        //Setzt die Labels auf den Code per Hashmapabfrage
+
+        fromCurrencys.setText(map.get(nameFrom));
+        toCurrencys.setText(map.get(nameTo));
+    }
+
+
     private void ConvertCurrency() {
         try{
-            String from = comboBoxFromCurrencies.getSelectedItem().toString();
-            String to = comboBoxToCurrencies.getSelectedItem().toString();
+            // Holt den namen der auf dem Dropdown steht um ihn anschliessend als Schlüssel der Hashmap abfrage zu nutzen um den "code" zu bekommen
+            //mit dem dann weiter gearbeiett wird
+            String fromDropdownName = comboBoxFromCurrencies.getSelectedItem().toString();
+            String toDropDownName =comboBoxToCurrencies.getSelectedItem().toString();
+
+            // Speichert den Währungscode in die variablen from und to
+            String from =map.get(fromDropdownName);
+            String to=map.get(toDropDownName);
+
+            //Schreibt den Währungscode in die Labels neben den Dropdown Menus
+            initializeCodeCurrencyLabels();
+
             double exchangeRate = CurrencyListFetcher.getExchangerate(from,to, "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/" );
 
             double amount = Double.parseDouble(textFieldCurrValueFrom.getText());
@@ -78,6 +120,38 @@ public class MainFrame extends JFrame {
         }catch(Exception e){
 
         }
+    }
+    private String getCodeFrom(){
+        try{
+            // Holt den namen der auf dem Dropdown steht um ihn anschliessend als Schlüssel der Hashmap abfrage zu nutzen um den "code" zu bekommen
+            //mit dem dann weiter gearbeiett wird
+            String fromDropdownName = comboBoxFromCurrencies.getSelectedItem().toString();
+
+            //Schreibt den Währungscode in die Labels neben den Dropdown Menus
+            fromCurrencys.setText(map.get(fromDropdownName));
+
+            // Gibt den Währungscode zurück
+            return map.get(fromDropdownName);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    private String getCodeTo(){
+        try{
+            // Holt den namen der auf dem Dropdown steht um ihn anschliessend als Schlüssel der Hashmap abfrage zu nutzen um den "code" zu bekommen
+            //mit dem dann weiter gearbeiett wird
+            String toDropdownName = comboBoxToCurrencies.getSelectedItem().toString();
+
+            //Schreibt den Währungscode in die Labels neben den Dropdown Menus
+            fromCurrencys.setText(map.get(toDropdownName));
+
+            // Gibt den Währungscode zurück
+            return map.get(toDropdownName);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
 
